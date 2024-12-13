1949f99a73f4bf2cc2d1f600f4c2bd03e8494726
keneogu <kc101com@gmail.com>
2024-12-13 18:33:15 +0100

    Get the content Id

--- src/main/java/com/kenedev/content_calender/controller/ContentController.java
+++ src/main/java/com/kenedev/content_calender/controller/ContentController.java
@@ -2,9 +2,12 @@ package com.kenedev.content_calender.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.kenedev.content_calender.model.Content;
 import com.kenedev.content_calender.repository.ContentCollectionRepository;
@@ -22,5 +25,10 @@ private final ContentCollectionRepository repository;
   @GetMapping("")
   public List<Content> findAll() {
     return repository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Content findById(@PathVariable Integer id) {
+    return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found"));
   }
 }
