a134f1fa1efc67e4efc0f15373d088b1adc934d6
keneogu <kc101com@gmail.com>
2024-12-13 13:32:54 +0100

    Start springboot project

---
+++ src/main/java/com/kenedev/content_calender/ContentCalenderApplication.java
@@ -0,0 +1,13 @@
+package com.kenedev.content_calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCalenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenedev/content_calender/ContentCalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenedev.content_calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
