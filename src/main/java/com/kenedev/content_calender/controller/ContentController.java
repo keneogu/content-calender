443718c85f1dcee7d81226decfb88bba9855bcab
keneogu <kc101com@gmail.com>
2024-12-14 18:30:51 +0100

    Add the create and update method controller

--- src/main/java/com/kenedev/content_calender/controller/ContentController.java
+++ src/main/java/com/kenedev/content_calender/controller/ContentController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -30,5 +34,20 @@ private final ContentCollectionRepository repository;
   @GetMapping("/{id}")
   public Content findById(@PathVariable Integer id) {
     return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found"));
+  }
+
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping("")
+  public void create(@RequestBody Content content) {
+    repository.save(content);
+  }
+
+  @PutMapping("/{id}")
+  public void update(@RequestBody Content content, @PathVariable Integer id) {
+    if(!repository.existsById(id)) {
+
+    }
+
+    repository.save(content);
   }
 }
