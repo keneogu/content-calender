36e137987457cab25d2e488e4bbba280412b7059
keneogu <kc101com@gmail.com>
2024-12-14 18:30:12 +0100

    Add the save, existsById method repository

--- src/main/java/com/kenedev/content_calender/repository/ContentCollectionRepository.java
+++ src/main/java/com/kenedev/content_calender/repository/ContentCollectionRepository.java
@@ -1,5 +1,6 @@
 package com.kenedev.content_calender.repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -7,23 +8,49 @@ import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 import com.kenedev.content_calender.model.Content;
+import com.kenedev.content_calender.model.Status;
+import com.kenedev.content_calender.model.Type;
+
+import jakarta.annotation.PostConstruct;
 
 @Repository
 public class ContentCollectionRepository {
-   
-  private final List<Content> content = new ArrayList<>();
+
+  private final List<Content> contentList = new ArrayList<>();
 
   public ContentCollectionRepository() {
 
   }
 
   public List<Content> findAll() {
-    return content;
+    return contentList;
   }
 
   public Optional<Content> findById(Integer id) {
-    return content.stream().filter(c -> c.id().equals(id)).findFirst();
+    return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
   }
 
-  
+  public void save(Content content) {
+    contentList.add(content);
+  }
+
+  @PostConstruct
+  private void init() {
+    Content c = new Content(1,
+        "My first blog post",
+        "This is my first blog post",
+        Status.IDEA,
+        Type.ARTICLE,
+        LocalDateTime.now(),
+        null,
+        "");
+
+    contentList.add(c);
+  }
+
+  public boolean existsById(Integer id) {
+    return contentList.stream().filter(c -> c.id().equals(id)).count() == 1;
+
+  }
+
 }
